6c89e0e438133d4fdbdeea720e36cf113e88dd7e
Stijn Tintel <stijn@linux-ipv6.be>
2023-09-06 14:36:13 +0300

    main: demonstrate recorder engine problem
    Minimal code to demonstrate a problem with the recorder engine.
    Initializing rec_config_t with the DEFAULT_REC_ENGINE_CONFIG() macro
    results in the following error:
    
    E (373) REC_ENG: Invalid parameters
    E (373) ESP_ADF_SKEL: failed to create recorder engine
    
    Steps to reproduce:
    
    podman run --rm -it --device /dev/ttyACM0 espressif/idf:v4.4.5
    
    cd /opt/esp
    git clone --depth=1 --recursive --shallow-submodules https://github.com/espressif/esp-adf.git adf
    cd adf
    export ADF_PATH=$PWD
    
    cd
    git clone -b bugreport/rec_engine https://github.com/toverainc/esp-adf-skel.git
    cd esp-adf-skel/adf
    
    echo -e "CONFIG_ESP32_S3_BOX_LITE_BOARD=y\nCONFIG_LOG_MAXIMUM_LEVEL_DEBUG=y" > sdkconfig.defaults
    idf.py set-target esp32s3
    idf.py build
    idf.py flash
    idf.py monitor

--- adf/main/main.c
+++ adf/main/main.c
@@ -1,6 +1,7 @@
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "recorder_engine.h"
 
 static const char *TAG = "ESP_ADF_SKEL";
 
@@ -9,6 +10,11 @@ void app_main(void)
 	esp_log_level_set("*", ESP_LOG_DEBUG);
 
 	ESP_LOGI(TAG, "starting");
+
+	rec_config_t cfg_re = DEFAULT_REC_ENGINE_CONFIG();
+	if (rec_engine_create(&cfg_re) != ESP_OK) {
+		ESP_LOGE(TAG, "failed to create recorder engine");
+	}
 
 	// main loop
 	while (true) {
